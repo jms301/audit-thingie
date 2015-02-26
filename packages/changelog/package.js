7a7be175f26ce090198a114f10b2531a7048146b
James Southren <jms301@gmail.com>
2015-02-26 08:44:11 +0000

    Add in changelog functionality applied to contracts

--- packages/changelog/package.js
+++ packages/changelog/package.js
@@ -1,8 +1,8 @@
 Package.describe({
-  name: 'contract',
+  name: 'changelog',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: 'Provides the contract feature.',
+  summary: '',
   // URL to the Git repository containing the source code for this package.
   git: '',
   // By default, Meteor will default to using README.md for documentation.
@@ -13,23 +13,21 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.0.3.1');
   api.use('standard-app-packages');
-  api.use('less');
-  api.use('twbs:bootstrap');
-  api.use('iron:router');
-  api.use('aslagle:reactive-table@0.3.21');
-  api.use('changelog');
 
-  api.addFiles('contract.less', 'client');
-  api.addFiles('contract.html', 'client');
+  api.use('matb33:collection-hooks');
+  api.addFiles('changelog.js');
+
   api.addFiles('setup.js');
   api.addFiles('server.js', 'server');
-  api.addFiles('contract.js', 'client');
+  api.addFiles('changelog.js', 'client');
 
-  api.export('Contracts');
+  api.export('ChangeLogFuncs', 'server');
+  api.export('ChangeLog', 'client');
+
 });
 
 Package.onTest(function(api) {
   api.use('tinytest');
-  api.use('contract');
-  api.addFiles('contract-tests.js');
+  api.use('changelog');
+  api.addFiles('changelog-tests.js');
 });
